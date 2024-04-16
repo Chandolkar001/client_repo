a8cc1268415634e33b208964b8bab376b616dfb8
Chandolkar001 <chandolkarshreyas001@gmail.com>
2024-04-16 16:33:32 +0530

    test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
 MAC="68:e4:3b:30:60:79"
 
 import os
-
+import sys
 # Example 1: Command Injection
 user_input = input("Enter a filename: ")
 os.system("cat " + user_input)
